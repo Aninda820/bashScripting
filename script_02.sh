ed5623fc393a39f2836ba44a0de05228f820c471
Aninda820 <michaelshroud01@gmail.com>
2024-06-18 21:03:15 +0530

    Comments in Bash

---
+++ script_02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Single Line comment"
+# This is an example of single line comments
+
+
+echo "Multiline comments"
+<<comment
+This the another example of 
+multiline commants
+comment

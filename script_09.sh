6dbb831cb2fdc19be0373e1b7e2a3c2c2e9b9208
Aninda820 <michaelshroud01@gmail.com>
2024-06-19 04:29:53 -0400

    User input, take input from users

---
+++ script_09.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# User Interaction
+
+# take input from user 
+# read <var_name>
+# read -p "your name" NAME
+
+read name
+echo "Enter your name billow!!"
+# echo "Hey $name, Welcome to hacking world"
+
+read -p "your name " NAME
+echo "Hey $NAME"
